60e82646e8d24dcc0338037b7701f15b6bbdf67d
Breyten Ernsting <bje@dds.nl>
2014-08-17 21:22:22 +0200

    Make lala configurable.

--- lala.rb
+++ lala.rb
@@ -1,17 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'bundler'
+
+require 'inifile'
 require 'nokogiri'
 require 'open-uri'
 require 'json'
 
-doc = Nokogiri::XML(open("http://rss.imdb.com/list/ls005182158/"))
-doc.css('guid').map{|t| t.content.split('/').last}.each_slice(50).each do |batch| 
+config = IniFile.load('lala.ini')
+doc = Nokogiri::XML(open(config["lala"]["watchlist"]))
+doc.css('guid').map{|t| t.content.split('/').last}.each_slice(50).each do |batch|
   # https://yts.re/api/listimdb.json?imdb_id[]=tt0499549&imdb_id[]=tt2024469&imdb_id[]=tt0114709
   r = open("https://yts.re/api/listimdb.json?imdb_id[]=" + batch.join('&imdb_id[]='))
   listimdb = JSON.parse r.read
   listimdb['MovieList'].each do |movie|
     match = /(\d+)p/.match(movie['Quality'])
-    if match && match[1].to_i > 710
+    if match && match[1].to_i > config["lala"]["min_quality"].to_i && match[1].to_i < config["lala"]["max_quality"]
       # you could call xdg-open movie["TorrentMagnetUrl"] here
-      puts movie['MovieTitle'] + ": " + movie["ImdbLink"] # + " - " + movie["TorrentMagnetUrl"] 
+      system("%s %s" % [config["lala"]["open_cmd"], movie["TorrentMagnetUrl"]]) unless config["lala"]["open_cmd"].nil?
+      puts movie['MovieTitle'] + ": " + movie["ImdbLink"] # + " - " + movie["TorrentMagnetUrl"]
     end
   end
 end
