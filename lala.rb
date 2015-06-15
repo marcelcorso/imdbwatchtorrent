1cb74d6537aaf4b702a87878095437c99eaad093
Marcel Corso <marcel.corso@gmail.com>
2015-06-15 20:46:48 +0000

    fixed. slower. dumber

--- lala.rb
+++ lala.rb
@@ -3,16 +3,13 @@ require 'open-uri'
 require 'json'
 
 doc = Nokogiri::XML(open("http://rss.imdb.com/list/ls005182158/"))
-doc.css('guid').map{|t| t.content.split('/').last}.each_slice(50).each do |batch| 
-  # https://yts.re/api/listimdb.json?imdb_id[]=tt0499549&imdb_id[]=tt2024469&imdb_id[]=tt0114709
-  r = open("https://yts.re/api/listimdb.json?imdb_id[]=" + batch.join('&imdb_id[]='))
-  listimdb = JSON.parse r.read
-  next unless listimdb["status"] != "fail"
-  listimdb['MovieList'].each do |movie|
-    match = /(\d+)p/.match(movie['Quality'])
-    if match && match[1].to_i > 710
-      # you could call xdg-open movie["TorrentMagnetUrl"] here
-      puts movie['MovieTitle'] + ": " + movie["ImdbLink"] # + " - " + movie["TorrentMagnetUrl"] 
-    end
-  end
+puts "got imdb list"
+doc.css('guid').map{|t| t.content.split('/').last}.each do |one| 
+  # https://yts.to/api/v2/list_movies.jsonp?query_term=tt0068615
+  r = open("https://yts.to/api/v2/list_movies.json?query_term="+one)
+  
+  parsed = JSON.parse r.read
+  next unless parsed["data"]["movie_count"].to_i > 0
+  movie  = parsed["data"]["movies"].first
+  puts movie['title_long'] + ": " + movie["url"] # + " - " + movie["TorrentMagnetUrl"] 
 end
